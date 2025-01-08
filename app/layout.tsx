e92e833c84039cdd4434e588b1af0e00962cd8a7
Solo Dev is Good <151962661+solo-fox@users.noreply.github.com>
2025-01-08 16:59:55 +0000

    chore(www): Inited a supabase app

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Geist } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+const geistSans = Geist({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={geistSans.className} className="bg-background text-foreground" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="h-screen flex flex-col gap-20 max-w-5xl p-5">{children}</div>
+          <footer className="w-full flex items-center justify-center mx-auto text-center text-xs gap-8 py-16">
+            <p>Powered by review.click</p>
+            <ThemeSwitcher />
+          </footer>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+}
