f2f4f26d054dd5f552bca78112342a9af1c16f12
Solo Dev is Good <151962661+solo-fox@users.noreply.github.com>
2025-01-11 11:09:09 +0000

    feat(ui/signup): Created the ui for the sign up page

---
+++ features/signup/components/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { SignUpForm } from "./signup-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen md:p-10">
+        <SignUpForm />
+    </div>
+  );
+}
