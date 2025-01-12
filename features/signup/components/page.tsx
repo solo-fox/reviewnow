41d7665d464968cf3d65833d9f3f526e54877351
Solo Dev is Good <151962661+solo-fox@users.noreply.github.com>
2025-01-12 12:11:10 +0000

    feat(ui/signup): Updated the ui to be more approachable

--- features/signup/components/page.tsx
+++ features/signup/components/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 
-import { SignUpCard } from "./signup-card";
+import SignUpForm  from "./signup-form";
 
 export default function SignUpPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen md:p-10">
-        <SignUpCard />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-1 items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-xs">
+          <SignUpForm />
+        </div>
+      </div>
     </div>
+
   );
 }
--- features/signup/components/signup-form.tsx
+++ features/signup/components/signup-form.tsx
@@ -13,56 +13,98 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import Link from "next/link"
-import useSignUp from "../hooks/use-signup"
+import Link from "next/link";
+import useSignUp from "../hooks/use-signup";
 import { Button } from "@/components/ui/button";
 import LoadingIcon from "@/components/loading-icon";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function SignUpForm() {
   const [pending, setPending] = useState<boolean>(false);
-  
+
   const signUpForm = useForm<z.infer<typeof signUpFormSchema>>({
     resolver: zodResolver(signUpFormSchema),
     defaultValues: {
       email: "",
-      password: ""
-    }
+      password: "",
+    },
   });
+
   const signUp = useSignUp(setPending);
 
   return (
-    <Form {...signUpForm} >
-      <form onSubmit={signUpForm.handleSubmit(signUp)} className="space-y-8">
-        <FormField
-          control={signUpForm.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="example@gmail.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={signUpForm.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="*******" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button disabled={pending} className="w-full" type="submit" >
-          {pending ? <LoadingIcon /> : ""} <p>Create a free account</p>
-        </Button>
+    <Form {...signUpForm}>
+      <form
+        onSubmit={signUpForm.handleSubmit(signUp)}
+        className={cn("flex flex-col gap-6")}
+      >
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Create your account</h1>
+          <p className="text-balance text-sm text-muted-foreground">
+            Enter your email and password to create a new account
+          </p>
+        </div>
+
+        <div className="grid gap-6">
+          <FormField
+            control={signUpForm.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    placeholder="example@gmail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={signUpForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="*******"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button disabled={pending} className="w-full" type="submit">
+            {pending ? <LoadingIcon /> : ""} <p>Create a free account</p>
+          </Button>
+
+          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+
+          <Button variant="outline" className="w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                fill="currentColor"
+              />
+            </svg>
+            Sign up with GitHub
+          </Button>
+        </div>
+
         <div className="text-center text-sm">
           Already have an account?{" "}
           <Link href="/signin" className="underline underline-offset-4">
