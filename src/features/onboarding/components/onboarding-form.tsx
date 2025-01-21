2fb6f6230da6491fcb9a429744aad98553b437e4
Solo Dev is Good <151962661+solo-fox@users.noreply.github.com>
2025-01-21 15:42:05 +0000

    feat(auth/onboarding): Created onboarding action

--- src/features/onboarding/components/onboarding-form.tsx
+++ src/features/onboarding/components/onboarding-form.tsx
@@ -15,17 +15,11 @@ import { z } from "zod";
 import { useState } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import onboardingSchema from "../schemas/onboarding.schema";
-import OrganizationName from "./organization-name";
+import LoadingIcon from "@/components/loading-icon";
+import onboardAction from "../actions/onboard.action"
 
 export default function OnboardingForm() {
   const [pending, setPending] = useState<boolean>(false);
-
-  const steps = [
-    {
-      component: OrganizationName,
-      action: () => {},
-    },
-  ];
 
   const onboardingForm = useForm<z.infer<typeof onboardingSchema>>({
     resolver: zodResolver(onboardingSchema),
@@ -40,7 +34,9 @@ export default function OnboardingForm() {
   ) {
     event?.preventDefault();
     setPending(true);
-
+    await onboardAction({
+      org_name: values.org_name,
+    });
     setPending(false);
   }
 
@@ -54,32 +50,30 @@ export default function OnboardingForm() {
 
       <Alert />
 
-      <CardContent className="flex flex-col gap-6 justify-center">
+      <CardContent>
         <Form {...onboardingForm}>
-          <form onSubmit={onboardingForm.handleSubmit(onSubmit)}>
+          <form
+            onSubmit={onboardingForm.handleSubmit(onSubmit)}
+            className="flex flex-col gap-6 justify-center"
+          >
             <FormField
               control={onboardingForm.control}
               name="org_name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Email</FormLabel>
+                  <FormLabel>Name of your organization</FormLabel>
                   <FormControl>
-                    <Input
-                      id="org_name"
-                      placeholder="Organization name"
-                      {...field}
-                    />
+                    <Input id="org_name" placeholder="ACME GmbH" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+            <Button disabled={pending} className="w-full" type="submit">
+              {pending ? <LoadingIcon /> : ""} <p>Create an organization</p>
+            </Button>
           </form>
         </Form>
-
-        <Button disabled={false} className="w-full" type="submit">
-          <p>Next</p>
-        </Button>
       </CardContent>
     </Card>
   );
