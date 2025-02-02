2e907f845111e2581b06e942b386c8a7edb0e4b5
A. Shaheen <151962661+solo-fox@users.noreply.github.com>
2025-02-02 19:10:13 +0000

    fix(ci/action): see #8. Apparently we must use <Suspense> so we can use nuqs. 😤

--- apps/web/features/error/components/page.tsx
+++ apps/web/features/error/components/page.tsx
@@ -9,28 +9,32 @@ import routes from "@/lib/routes";
 import { useRouter } from "next/navigation";
 import { Suspense } from "react";
 
-export default function ErrorPage() {
-  const [message, _] = useQueryState("message");
-  const router = useRouter();
-  if (!message) return router.push(routes.home);
-
+function ErrorContent() {
+  const [message] = useQueryState("message", { defaultValue: "" })
+  
   return (
-    <Suspense>
-      <div className="min-h-svh flex flex-col justify-center items-center p-6 gap-6">
-        <div className="dotted-background"></div>
-        <Illustration />
-        <h1 className="mt-6 font-bold tracking-tight sm:text-4xl">
-          An unexxxpected error!
-        </h1>
-        <Alert message={message} isError={true} />
-        <div className="flex items-center justif-center p-6 gap-2">
-          <Link href={routes.home}>Home</Link>
-          <p>•</p>
-          <Link href={routes.resources.contact}>Contact</Link>
-          <p>•</p>
-          <Link href={routes.resources.docs}>Docs</Link>
-        </div>
+    <div className="min-h-svh flex flex-col justify-center items-center p-6 gap-6">
+      <div className="dotted-background"></div>
+      <Illustration />
+      <h1 className="mt-6 font-bold tracking-tight sm:text-4xl">
+        An unexpected error!
+      </h1>
+      <Alert message={message} isError={true} />
+      <div className="flex items-center justify-center p-6 gap-2">
+        <Link href={routes.home}>Home</Link>
+        <p>•</p>
+        <Link href={routes.resources.contact}>Contact</Link>
+        <p>•</p>
+        <Link href={routes.resources.docs}>Docs</Link>
       </div>
+    </div>
+  );
+}
+
+export default function ErrorPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorContent />
       <Footer />
     </Suspense>
   );
